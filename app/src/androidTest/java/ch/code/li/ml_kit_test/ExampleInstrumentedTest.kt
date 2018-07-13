37c8f6c840cc43e3570656581bad63b8f73ddabb
Luecu <luca.indermuehle@gmail.com>
2018-07-13 21:56:47 +0200

    Initial commit

---
+++ app/src/androidTest/java/ch/code/li/ml_kit_test/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package ch.code.li.ml_kit_test
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("ch.code.li.ml_kit_test", appContext.packageName)
+    }
+}
